91f0e6ba802ac920d388da3a17028e773ef156cd
David Weinstein <txea@Davids-MacBook-Air.local>
2016-12-20 19:06:15 -0500

    day two

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,7 +15,7 @@ class FiguresController < ApplicationController
   end
 
   get '/figures' do
-    binding.pry
+    # binding.pry
 
     @figures = Figure.all
     @titles = Title.all
@@ -23,7 +23,36 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
 
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    l_ids  = params[:figure][:landmark_ids]
+    l_ids.each do |l|
+      @figure.landmarks << Landmark.find_or_create_by(l.to_i)
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+    @figure.save
+    # binding.pry
+
+
+# binding.pry
+    # @title = Title.create(params[:title])
+    #
+    # binding.pry
+    #
+    # @figure.titles << @title
+    # @figure.save
+    # @figure.titles = params[:figure][:title_ids]
+  end
+  # => {"figure"=>{"name"=>"h", "title_ids"=>["1"], "landmark_ids"=>["3"]}, "title"=>{"name"=>"test"}, "landmark"=>{"name"=>"test2"}}
+
+
   get '/figures/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/new'
   end
 end
